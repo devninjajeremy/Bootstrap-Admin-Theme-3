84929ee84d448f528c11563c87a2388efafab411
Jeremy Davis <ampthismusic@gmail.com>
2015-04-27 19:24:17 -0400

    Theme changes

---
+++ includes/mast_header.php
@@ -0,0 +1,22 @@
+<div class="header navbar-fixed-top">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-5">
+			<!-- Logo -->
+				<div class="logo">
+					<h1><a href="<?= base_url(); ?>">Bootstrap Admin Theme</a></h1>
+				</div>
+			</div>
+			<div class="col-md-5">
+				<div class="row">
+					<div class="col-lg-12">
+						<?= modules::run('geonosis/search'); ?>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-2">
+				<?= modules::run('geonosis/utility_nav'); ?>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ includes/search.php
@@ -0,0 +1,6 @@
+<div class="input-group form">
+   <input type="text" class="form-control" placeholder="Search...">
+   <span class="input-group-btn">
+     <button class="btn btn-primary" type="button">Search</button>
+   </span>
+</div>
